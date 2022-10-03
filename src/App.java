636f5b17511f1333f3090ada8fd8933b31399c58
Megyeri Márk <megyko1234@gmail.com>
2022-10-03 11:15:08 +0200

    Buborék rendezés

---
+++ src/App.java
@@ -0,0 +1,12 @@
+public class App {
+
+
+    public static void  bubiRendez(){
+
+    }
+    public static void main(String[] args) throws Exception {
+        System.out.println("Buborék rendezés");
+        int[] szamok={39,25,12,48,27,38};
+        bubiRendez();
+    }
+}
