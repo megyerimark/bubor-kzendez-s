ea35d8151a73e808b6b1d616dd6c847556a48795
Megyeri Márk <megyko1234@gmail.com>
2022-10-03 11:33:09 +0200

    rendezés metódus refaktorálása

--- src/App.java
+++ src/App.java
@@ -2,7 +2,7 @@ public class App {
 
 
     public static void  bubiRendez(int[] szamok){
-        for (int i =  szamok.length-1 ; i < szamok.length; i++) {
+        for (int i =  szamok.length-1 ; i > szamok.length; i--) {
             for (int j = 0; j < i; j++) {
                 if(szamok[j]>szamok[j+1]){
                     int csere =szamok[j];
@@ -33,5 +33,6 @@ System.out.println();
         System.out.println("Buborék rendezés");
         int[] szamok={39,25,12,48,27,38};
         bubiRendez(szamok);
+        kiirTomb(szamok);
     }
 }
