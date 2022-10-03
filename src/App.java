41ab1d48929a62a2a0928943b9630fdaf0a24d5e
Megyeri Márk <megyko1234@gmail.com>
2022-10-03 11:21:24 +0200

    Csere művelet

--- src/App.java
+++ src/App.java
@@ -3,10 +3,22 @@ public class App {
 
     public static void  bubiRendez(int [] szamok){
         for (int i =  szamok.length-1 ; i < szamok.length; i++) {
+            for (int j = 0; j < i; j++) {
+                if(szamok[j]>szamok[j+1]){
+                    int csere =szamok[j];
+                    szamok[j] = szamok[j+1];
+                    szamok[j+1]= csere;
+                }
+                
+            }
             
         }
 
     }
+
+
+
+
     public static void main(String[] args) throws Exception {
         System.out.println("Buborék rendezés");
         int[] szamok={39,25,12,48,27,38};
