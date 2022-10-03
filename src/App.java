d82c9542a584b036d93dd54087b2b8e638543a88
Megyeri Márk <megyko1234@gmail.com>
2022-10-03 11:18:57 +0200

    For ciklus kezdete

--- src/App.java
+++ src/App.java
@@ -1,12 +1,15 @@
 public class App {
 
 
-    public static void  bubiRendez(){
+    public static void  bubiRendez(int [] szamok){
+        for (int i =  szamok.length-1 ; i < szamok.length; i++) {
+            
+        }
 
     }
     public static void main(String[] args) throws Exception {
         System.out.println("Buborék rendezés");
         int[] szamok={39,25,12,48,27,38};
-        bubiRendez();
+        bubiRendez(szamok);
     }
 }
